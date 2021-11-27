5ddf416cda533cb6e90234fddf40f4854cefd4f6
Steven Klein <sklein2@oakland.edu>
2021-11-26 20:25:27 -0500

    added .ejs files, modified existing files

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-const express = require("express");
+const express = require('express');
 const ejs = require("ejs");
-//const http = require('http');
-//const path = require('path');
-//const fs = require('fs');
+// const http = require('http');
+// const path = require('path');
+// const fs = require('fs');
 
 
 //create express app
-const app = express();
+const app = new express();
 
 //Initialize Body Parser Middleware to parse data sent by users
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Initialize EJS Middleware
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
+// app.set('views', path.join(__dirname, '/public')); //maybe don't use
 
 //Begin Server Logic
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get("/html/resume", (req, res) => {
+    res.render('/html/myresume');
+});
+
+app.get("/html/funfacts", (req, res) => {
+    res.render('/html/myfunfacts');
+});
+
+app.get('/trivia', (req, res) => {
+    res.render('/html/trivia');
 });
 
 //Start Server
