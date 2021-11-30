5d737e2cf807035e3e774cc0b4bdd9fe872917bd
Steven Klein <sklein2@oakland.edu>
2021-11-29 23:05:34 -0500

    modified files, adjusted routes and paths

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const express = require('express');
+const express = require("express");
 const ejs = require("ejs");
-// const http = require('http');
-const path = require('path');
-// const fs = require('fs');
-
 
 //create express app
 const app = new express();
@@ -13,29 +9,100 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Initialize EJS Middleware
-
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
-app.set('views', path.join(__dirname, 'views')); //maybe don't use
+//app.set('views', path.join(__dirname, 'views')); //maybe don't use
 //app.set('views', path.join(__dirname, '/public')); //maybe don't use
 
-
 //Begin Server Logic
-app.get('/', (req, res) => {
-    res.render('index');
+//routes
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.get("/resume", (req, res) => {
-    res.render('/html/my_resume');
+    res.render("./html/resume");
 });
 
-app.get("/myfunfacts", (req, res) => {
-    res.render('/html/my_fun_facts');
+app.get('/funFacts', (req, res) => {
+    res.render('./html/funfacts');
 });
 
 app.get('/trivia', (req, res) => {
-    res.render('/html/trivia');
+    res.render('./html/trivia');
 });
+
+// let score = 0;
+
+// app.post("/trivia", (req, res) => {
+//     let data = {answer: req.body.trivia1};
+//     let correct1 = 'B';
+//     if(trivia1 === correct1)
+//     {
+//         score++;
+//         res.send(`Correct!`);
+//     }
+//     else
+//     {
+//         res.send(`Unfortunately, that is incorrect.`);
+//     }
+// });
+// app.post("/trivia", (req, res) => {
+//     let data = {answer: req.body.trivia2};
+//     let correct2 = 'C';
+//     if(trivia2 === correct2)
+//     {
+//         score++;
+//         res.send(`Correct!`);
+//     }
+//     else
+//     {
+//         res.send(`Unfortunately, that is incorrect.`);
+//     }
+// });
+// app.post("/trivia", (req, res) => {
+//     let data = {answer: req.body.trivia3};
+//     let correct3 = 'D';
+//     if(trivia3 === correct3)
+//     {
+//         score++;
+//         res.send(`Correct!`);
+//     }
+//     else
+//     {
+//         res.send(`Unfortunately, that is incorrect.`);
+//     }
+// });
+// app.post("/trivia", (req, res) => {
+//     let data = {answer: req.body.trivia4};
+//     let correct4 = 'A';
+//     if(trivia4 === correct4)
+//     {
+//         score++;
+//         res.send(`Correct!`);
+//     }
+//     else
+//     {
+//         res.send(`Unfortunately, that is incorrect.`);
+//     }
+// });
+// app.post("/trivia", (req, res) => {
+//     let data = {answer: req.body.trivia5};
+//     let correct5 = 'C';
+//     if(trivia5 === correct5)
+//     {
+//         score++;
+//         res.send(`Correct!`);
+//     }
+//     else
+//     {
+//         res.send(`Unfortunately, that is incorrect.`);
+//     }
+// });
+
+// app.post("/trivia", (req, res) => {
+//     res.send(score);
+// });
 
 //Start Server
 const PORT = process.env.PORT || 3000;
