92fc71bdf9c61847854e25795bc00024f96b436e
Steven Klein <sklein2@oakland.edu>
2021-12-02 21:56:40 -0500

    added css styling, modied index.js, trivia.ejs

--- index.js
+++ index.js
@@ -32,6 +32,139 @@ app.get('/trivia', (req, res) => {
     res.render('./html/trivia');
 });
 
+app.post('/questions/', (req, res) => {
+    let score = 0;
+    
+    let data1 = req.body.answer1;
+    let trivia1 = 'B';
+    if (data1 == trivia1)
+    {
+        score++;
+        // res.send(`Great Job!`);
+    }
+    else
+    {
+        // res.send(`Better Luck Next Time`);
+    }
+    //*************************************************************************
+    let data2 = req.body.answer2;
+    let trivia2 = 'C';
+    if (data2 == trivia2)
+    {
+        score++;
+        // res.send(`Great Job!`);
+    }
+    else
+    {
+        // res.send(`Better Luck Next Time`);
+    }
+    //*************************************************************************/
+    let data3 = req.body.answer3;
+    let trivia3 = 'D';
+    if (data3 == trivia3)
+    {
+        score++;
+        // res.send(`Great Job!`);
+    }
+    else
+    {
+        // res.send(`Better Luck Next Time`);
+    }
+    //************************************************************************** */
+    let data4 = req.body.answer4;
+    let trivia4 = 'A';
+    if (data4 == trivia4)
+    {
+        score++;
+        // res.send(`Great Job!`);
+    }
+    else
+    {
+        // res.send(`Better Luck Next Time`);
+    }
+    //****************************************************************************** */
+    let data5 = req.body.answer5;
+    let trivia5 = 'C';
+    if (data5 == trivia5)
+    {
+        score++;
+        // res.send(`Great Job!`);
+    }
+    else
+    {
+        // res.send(`Better Luck Next Time`);
+    }
+    // res.send(data);
+    // res.send(data1);
+    // res.send(`${data}`);
+    console.log(score);
+});
+//********************************************************** */
+// app.get('/score/', (req, res) => {
+//     res.render("trivia", { score: finalScore });
+// });
+//************************************************************* */
+// app.post('/trivia/questions', (req, res) => {
+//     let score = 0;
+//     let idk = document.querySelector('#question1');
+//     console.log(idk);
+//     let ans1 = 'B';
+//     let data = { answer: req.body.answer1 };
+//     if(data === ans1)
+//     {
+//         score++;
+//         res.send('Correct!');
+//         console.log(`${score}`);
+//     }
+//     else
+//     {
+//         res.send('Better Luck Next Time');
+//     }
+//     // let ans2 = 'C';
+//     // let data = { answer: req.body.answer2 };
+
+//     // let ans3 = 'D';
+//     // let data = { answer: req.body.answer3 };
+
+//     // let ans4 = 'A';
+//     // let data = { answer: req.body.answer4 };
+
+//     // let ans5 = 'C';
+//     // let data = { answer: req.body.answer5 };
+    
+//     let finalScore = document.querySelector('#score');
+//     finalScore.addEventListener('click', buttonClicked);
+//     function buttonClicked()
+//     {
+//         res.send(`Your Score: ${score}`);
+//     }
+// });
+
+
+
+// app.post('/trivia/url', (req, res) => {
+//     let score = 0;
+//     let answer5 = 'C';
+//     let user = `${req.body.input}`;
+//     if(user === answer5){
+//         score++;
+//         res.send("Correct!");
+//     }
+//     else{
+//         res.send("Better Luck Next Time");
+//     }
+
+// });
+
+// app.post('/trivia/score', (req, res) => {
+//     let finalScore = document.querySelector('#score');
+//     finalScore.addEventListener('click', buttonClicked);
+//     function buttonClicked()
+//     {
+//         res.send(score);
+//     }
+
+// })
 // let score = 0;
 
 // app.post("/trivia", (req, res) => {
