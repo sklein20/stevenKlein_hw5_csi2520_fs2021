75d65daeebbfbe55d6dd7e7ed4439ad66f42d568
Steven Klein <sklein2@oakland.edu>
2021-12-04 00:58:31 -0500

    modified css, index.js, trivia.ejs

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 //Initialize EJS Middleware
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
-//app.set('views', path.join(__dirname, 'views')); //maybe don't use
-//app.set('views', path.join(__dirname, '/public')); //maybe don't use
 
 //Begin Server Logic
 //routes
@@ -32,9 +30,8 @@ app.get('/trivia', (req, res) => {
     res.render('./html/trivia');
 });
 
-app.post('/questions/', (req, res) => {
+app.post('/url', (req, res) => {
     let score = 0;
-    
     let data1 = req.body.answer1;
     let trivia1 = 'B';
     if (data1 == trivia1)
@@ -94,148 +91,9 @@ app.post('/questions/', (req, res) => {
     {
         // res.send(`Better Luck Next Time`);
     }
-    // res.send(data);
-    // res.send(data1);
-    // res.send(`${data}`);
     console.log(score);
+    res.send({score: score});
 });
-//********************************************************** */
-// app.get('/score/', (req, res) => {
-//     res.render("trivia", { score: finalScore });
-// });
-//************************************************************* */
-// app.post('/trivia/questions', (req, res) => {
-//     let score = 0;
-//     let idk = document.querySelector('#question1');
-//     console.log(idk);
-//     let ans1 = 'B';
-//     let data = { answer: req.body.answer1 };
-//     if(data === ans1)
-//     {
-//         score++;
-//         res.send('Correct!');
-//         console.log(`${score}`);
-//     }
-//     else
-//     {
-//         res.send('Better Luck Next Time');
-//     }
-//     // let ans2 = 'C';
-//     // let data = { answer: req.body.answer2 };
-
-//     // let ans3 = 'D';
-//     // let data = { answer: req.body.answer3 };
-
-//     // let ans4 = 'A';
-//     // let data = { answer: req.body.answer4 };
-
-//     // let ans5 = 'C';
-//     // let data = { answer: req.body.answer5 };
-    
-//     let finalScore = document.querySelector('#score');
-//     finalScore.addEventListener('click', buttonClicked);
-//     function buttonClicked()
-//     {
-//         res.send(`Your Score: ${score}`);
-//     }
-// });
-
-
-
-// app.post('/trivia/url', (req, res) => {
-//     let score = 0;
-//     let answer5 = 'C';
-//     let user = `${req.body.input}`;
-//     if(user === answer5){
-//         score++;
-//         res.send("Correct!");
-//     }
-//     else{
-//         res.send("Better Luck Next Time");
-//     }
-
-// });
-
-// app.post('/trivia/score', (req, res) => {
-//     let finalScore = document.querySelector('#score');
-//     finalScore.addEventListener('click', buttonClicked);
-//     function buttonClicked()
-//     {
-//         res.send(score);
-//     }
-
-// })
-// let score = 0;
-
-// app.post("/trivia", (req, res) => {
-//     let data = {answer: req.body.trivia1};
-//     let correct1 = 'B';
-//     if(trivia1 === correct1)
-//     {
-//         score++;
-//         res.send(`Correct!`);
-//     }
-//     else
-//     {
-//         res.send(`Unfortunately, that is incorrect.`);
-//     }
-// });
-// app.post("/trivia", (req, res) => {
-//     let data = {answer: req.body.trivia2};
-//     let correct2 = 'C';
-//     if(trivia2 === correct2)
-//     {
-//         score++;
-//         res.send(`Correct!`);
-//     }
-//     else
-//     {
-//         res.send(`Unfortunately, that is incorrect.`);
-//     }
-// });
-// app.post("/trivia", (req, res) => {
-//     let data = {answer: req.body.trivia3};
-//     let correct3 = 'D';
-//     if(trivia3 === correct3)
-//     {
-//         score++;
-//         res.send(`Correct!`);
-//     }
-//     else
-//     {
-//         res.send(`Unfortunately, that is incorrect.`);
-//     }
-// });
-// app.post("/trivia", (req, res) => {
-//     let data = {answer: req.body.trivia4};
-//     let correct4 = 'A';
-//     if(trivia4 === correct4)
-//     {
-//         score++;
-//         res.send(`Correct!`);
-//     }
-//     else
-//     {
-//         res.send(`Unfortunately, that is incorrect.`);
-//     }
-// });
-// app.post("/trivia", (req, res) => {
-//     let data = {answer: req.body.trivia5};
-//     let correct5 = 'C';
-//     if(trivia5 === correct5)
-//     {
-//         score++;
-//         res.send(`Correct!`);
-//     }
-//     else
-//     {
-//         res.send(`Unfortunately, that is incorrect.`);
-//     }
-// });
-
-// app.post("/trivia", (req, res) => {
-//     res.send(score);
-// });
 
 //Start Server
 const PORT = process.env.PORT || 3000;
